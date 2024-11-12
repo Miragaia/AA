275ba1280b1dee6452e6f81800d480362645e6a0
miragaia <miguelmiragaia2003@gmail.com>
2024-11-12 11:30:26 +0000

    algorithm update

--- projeto1/algorithms.py
+++ projeto1/algorithms.py
@@ -8,39 +8,29 @@ def precompute_edge_dict(G):
 def is_edge_dominating_set(edge_dict, edge_set, all_edges):
     covered_edges = set()
 
-    for u, v, _ in edge_set:  # Now that edge_set includes (u, v, weight), no unpacking error
+    for u, v, _ in edge_set:
         covered_edges |= edge_dict[u]
         covered_edges |= edge_dict[v]
 
-        if len(covered_edges) == all_edges:
-            return True
-
-    return False
+    return len(covered_edges) == all_edges
 
 def exhaustive_search_mweds(G):
     min_weight = float('inf')
     min_weight_set = []
     
-    # Retrieve edges with weights using data=True
-    edges = list(G.edges(data=True))  # List of (u, v, weight) tuples
+    edges = list(G.edges(data=True))  # Includes edge weights
     all_edges = G.number_of_edges()
-
     edge_dict = precompute_edge_dict(G)
 
     for r in range(1, all_edges + 1):
         for edge_subset in combinations(edges, r):
             if is_edge_dominating_set(edge_dict, edge_subset, all_edges):
-                weight = sum(w['weight'] for u, v, w in edge_subset)  # Sum weights
+                weight = sum(w['weight'] for u, v, w in edge_subset)
                 if weight < min_weight:
                     min_weight = weight
                     min_weight_set = edge_subset
-                    break
-        if min_weight_set:
-            break
 
     return min_weight_set, min_weight
-
-
 
 def greedy_mweds(G):
     edge_list = sorted(G.edges(data='weight'), key=lambda x: x[2])
@@ -53,6 +43,7 @@ def greedy_mweds(G):
             covered_edges.update(G.edges(u))
             covered_edges.update(G.edges(v))
 
+            # Stop once all edges are covered
             if len(covered_edges) >= len(G.edges):
                 break
 
