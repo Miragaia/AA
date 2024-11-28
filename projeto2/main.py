c701689cd8ce41fa5f95766e1fdd02b96fcef598
miragaia <miguelmiragaia2003@gmail.com>
2024-11-28 11:59:48 +0000

    updated main

--- projeto2/main.py
+++ projeto2/main.py
@@ -41,7 +41,7 @@ def draw_and_save_graph(graph_file, edge_set, num_vertices, percentage, algorith
 
 def main():
     results_randomized = []
-    results_randomized_heuristic = []
+    results_dynamic = []
     comparison_results = []
 
     vertices_num_last_graph, max_value_coordinate = read_arguments()
@@ -72,64 +72,64 @@ def main():
             'randomized_configurations': randomized_configurations
         })
         
-        # randomized_heuristic Heuristic with profiling
+        # dynamic with profiling
         start_time = time.time()
-        randomized_heuristic_set, randomized_heuristic_weight, randomized_heuristic_operations, randomized_heuristic_configurations = dynamic_randomized_mweds(G)
+        dynamic_set, dynamic_weight, dynamic_operations, dynamic_configurations = dynamic_randomized_mweds(G)
         end_time = time.time()
-        randomized_heuristic_time = end_time - start_time
-        results_randomized_heuristic.append({
+        dynamic_time = end_time - start_time
+        results_dynamic.append({
             'vertices_num': num_vertices,
             'percentage_max_num_edges': edge_prob,
-            'total_weight': randomized_heuristic_weight,
-            'solution_size': len(randomized_heuristic_set),
-            'execution_time': randomized_heuristic_time,
-            'num_operations': randomized_heuristic_operations,
-            'randomized_heuristic_configurations': randomized_heuristic_configurations
+            'total_weight': dynamic_weight,
+            'solution_size': len(dynamic_set),
+            'execution_time': dynamic_time,
+            'num_operations': dynamic_operations,
+            'dynamic_configurations': dynamic_configurations
         })
 
         comparison_results.append({
                 'vertices_num': num_vertices,
                 'percentage_max_num_edges': edge_prob,
                 'randomized_total_weight': randomized_weight,
-                'randomized_heuristic_total_weight': randomized_heuristic_weight,
-                'weight_difference': randomized_weight - randomized_heuristic_weight,
+                'dynamic_total_weight': dynamic_weight,
+                'weight_difference': randomized_weight - dynamic_weight,
                 'randomized_execution_time': randomized_time,
-                'randomized_heuristic_execution_time': randomized_heuristic_time,
-                'time_ratio': randomized_heuristic_time / randomized_time if randomized_time != 0 else float('inf'),
+                'dynamic_execution_time': dynamic_time,
+                'time_ratio': dynamic_time / randomized_time if randomized_time != 0 else float('inf'),
                 'randomized_num_operations': randomized_operations,
-                'randomized_heuristic_num_operations': randomized_heuristic_operations,
+                'dynamic_num_operations': dynamic_operations,
                 'randomized_configurations': randomized_configurations,
-                'randomized_heuristic_configurations': randomized_heuristic_configurations
+                'dynamic_configurations': dynamic_configurations
             })
 
         # Draw and save graphs with marked solutions
         randomized_edges = [(u, v) for u, v, w in randomized__set]
-        randomized_heuristic_edges = [(u, v) for u, v, w in randomized_heuristic_set]
+        dynamic_edges = [(u, v) for u, v, w in dynamic_set]
         
         if num_vertices <= 8:
             draw_and_save_graph(
                 graph_file, randomized_edges, num_vertices, edge_prob, "randomized", "Randomized Solution")
             
             draw_and_save_graph(
-                graph_file, randomized_heuristic_edges, num_vertices, edge_prob, "randomized_heuristic", "Randomized Heuristic Solution")
+                graph_file, dynamic_edges, num_vertices, edge_prob, "dynamic", "Randomized Heuristic Solution")
 
     df_randomized = pd.DataFrame(results_randomized)
-    df_randomized_heuristic = pd.DataFrame(results_randomized_heuristic)
+    df_dynamic = pd.DataFrame(results_dynamic)
     df_comparison = pd.DataFrame(comparison_results)
 
     save_to_csv(df_randomized, "randomized_results.csv")
-    save_to_csv(df_randomized_heuristic, "randomized_heuristic_results.csv")
+    save_to_csv(df_dynamic, "dynamic_results.csv")
     save_to_csv(df_comparison, "comparison_results.csv")
     
     # # Generate plots and analyses
     # executions_times(df_randomized, "Randomized Search")
-    # executions_times(df_randomized_heuristic, "Randomized Heuristic")
+    # executions_times(df_dynamic, "Randomized Heuristic")
     # basic_operations_num(df_randomized, "Randomized Search")
-    # basic_operations_num(df_randomized_heuristic, "Randomized Heuristic")
-    # basic_operations_num_aggregated(df_randomized, df_randomized_heuristic)
-    # plot_time_complexity(df_randomized, df_randomized_heuristic)
+    # basic_operations_num(df_dynamic, "Randomized Heuristic")
+    # basic_operations_num_aggregated(df_randomized, df_dynamic)
+    # plot_time_complexity(df_randomized, df_dynamic)
     # compare_solutions(df_comparison)
-    # predict_large_graph_times_75(df_randomized, df_randomized_heuristic, [8, 9, 10, 15, 20, 25, 30])
+    # predict_large_graph_times_75(df_randomized, df_dynamic, [8, 9, 10, 15, 20, 25, 30])
     # predict_large_graph_space([8, 9, 10, 15, 20, 25, 30])
     # greedy_weight_accuracy(df_comparison)
 
